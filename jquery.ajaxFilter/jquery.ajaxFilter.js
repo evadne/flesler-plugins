3ab3676f7c41857140179bb3852d05034e121662
Ariel Flesler <aflesler@gmail.com>
2009-06-07 15:06:59 +0000

    * Fixing a misc thing in scrollTo's demo   * Adding 2 features to ajaxFilter.

--- jquery.ajaxFilter/jquery.ajaxFilter.js
+++ jquery.ajaxFilter/jquery.ajaxFilter.js
@@ -11,28 +11,37 @@
  */
 ;(function( $ ){
 	
-	var filters = { };
+	var registry = { };
 
 	$.ajaxFilter = {
 		// Expose it just in case
-		f : filters,
+		filters : registry,
 		// types can be any of script, json, xml, html
 		// or many separated by spaces
 		register:function( name, types, fn ){
 			$.each( types.split(' '), function( i, type ){
-				if( !filters[type] )
-					filters[type] = {};
-				filters[type][name] = fn;
+				if( !registry[type] )
+					registry[type] = {};
+				registry[type][name] = fn;
 			});
+		},
+		// This could be used without the need of an ajax request
+		run:function run(type, filter, data){
+			var filter = registry[type] && registry[type][ filter ];
+
+			// The 'this' of the function will be the settings object
+			return filter && typeof data === 'string'? filter.call( this, data, type ) : data;
 		}
 	};
 	
 	// This handler is used instead, don't override it
 	$.ajaxSettings.dataFilter = function( data, type ){
-		var filter = filters[type] && filters[type][ this.filter ];
-
-		// The 'this' of the function will be the settings object
-		return filter ? filter.call( this, data, type ) : data;
+		if (this.filter == '*')
+			for( var filter in registry[type] )
+				data = $.ajaxFilter.run( type, filter, data )
+		else
+			data = $.ajaxFilter.run( type, this.filter, data );
+		return data;
 	};
 	
 })( jQuery );
